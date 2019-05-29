906c60fbcd844629b3a31bce2257517246bab4f8
Fabiano Gonzaga <f.augusto.g@gmail.com>
2019-05-29 11:40:19 -0300

    Importação inicial

---
+++ Programa para enviar email/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPara = new System.Windows.Forms.TextBox();
+            this.txtAssunto = new System.Windows.Forms.TextBox();
+            this.txtAnexo = new System.Windows.Forms.TextBox();
+            this.btnLocalizar = new System.Windows.Forms.Button();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ofdProcurar = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(596, 115);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(488, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Programa para enviar e-mail";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Para:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(52, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Assunto:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(52, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Anexo";
+            // 
+            // txtPara
+            // 
+            this.txtPara.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPara.Location = new System.Drawing.Point(124, 220);
+            this.txtPara.Name = "txtPara";
+            this.txtPara.Size = new System.Drawing.Size(418, 26);
+            this.txtPara.TabIndex = 5;
+            // 
+            // txtAssunto
+            // 
+            this.txtAssunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAssunto.Location = new System.Drawing.Point(124, 314);
+            this.txtAssunto.Name = "txtAssunto";
+            this.txtAssunto.Size = new System.Drawing.Size(418, 26);
+            this.txtAssunto.TabIndex = 6;
+            // 
+            // txtAnexo
+            // 
+            this.txtAnexo.Enabled = false;
+            this.txtAnexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnexo.Location = new System.Drawing.Point(124, 268);
+            this.txtAnexo.Name = "txtAnexo";
+            this.txtAnexo.Size = new System.Drawing.Size(286, 26);
+            this.txtAnexo.TabIndex = 8;
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocalizar.Location = new System.Drawing.Point(431, 268);
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(111, 26);
+            this.btnLocalizar.TabIndex = 9;
+            this.btnLocalizar.Text = "Localizar";
+            this.btnLocalizar.UseVisualStyleBackColor = true;
+            this.btnLocalizar.Click += new System.EventHandler(this.btnLocalizar_Click);
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensagem.Location = new System.Drawing.Point(56, 401);
+            this.txtMensagem.Multiline = true;
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(486, 174);
+            this.txtMensagem.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(52, 366);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Mensagem:";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(431, 600);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(111, 26);
+            this.btnEnviar.TabIndex = 12;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(124, 175);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(418, 26);
+            this.txtNome.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(52, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Nome:";
+            // 
+            // ofdProcurar
+            // 
+            this.ofdProcurar.FileName = "ofdProcurar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 647);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnLocalizar);
+            this.Controls.Add(this.txtAnexo);
+            this.Controls.Add(this.txtAssunto);
+            this.Controls.Add(this.txtPara);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(612, 686);
+            this.MinimumSize = new System.Drawing.Size(612, 686);
+            this.Name = "Form1";
+            this.Text = "Programa para enviar e-mail";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPara;
+        private System.Windows.Forms.TextBox txtAssunto;
+        private System.Windows.Forms.TextBox txtAnexo;
+        private System.Windows.Forms.Button btnLocalizar;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.OpenFileDialog ofdProcurar;
+    }
+}
+
---
+++ Programa para enviar email/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage Mail = new MailMessage();
+                //Digite aqui o servidor SMTP para a aplicação acessar
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                //A porta de acesso
+                int port = 587;
+
+                string MeuEmail = "seuEmail@gmail.com";
+                string MeuPassword = "senha";
+
+                //Digite aqui o e-mail que enviara a mensagem e o nome mostrado(remetente)
+                Mail.From = new MailAddress(MeuEmail, "Seu nome");
+                //Digite o e-amil da pessoa que vai recebe (destinatario)
+                Mail.To.Add(txtPara.Text);
+                //Nivel de prioridade da mensagem
+                Mail.Priority = MailPriority.High;
+                //Assunto da mensagem
+                Mail.Subject = txtAssunto.Text;
+
+                //Corpo da página com o recurso html ligado.
+                Mail.Body = corpoDaMensagem(txtNome.Text, txtMensagem.Text);
+
+                //Permite que seja inserido uma estrutura html na mensagem.
+                Mail.IsBodyHtml = true;
+
+                //Anexa arquivo na mensagem
+                if (txtAnexo.Text != "") Mail.Attachments.Add(new Attachment(txtAnexo.Text));
+
+                //Porta utilizada no servidor SMTP
+                SmtpServer.Port = port;
+
+                //Digite aqui a credencial de e-mail e senha válidos para acessar.
+                SmtpServer.Credentials = new System.Net.NetworkCredential(MeuEmail, MeuPassword);
+                //A autenticação de segura do e-mail - SSL
+                SmtpServer.EnableSsl = true;
+                //SmtpServer.Timeout = 10000;
+                SmtpServer.Send(Mail);
+
+                string a = txtNome.Text;
+
+                limparCaixasDeTextos();
+
+                MessageBox.Show("E-mail enviado com sucesso para " + a + "!", "Pronto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch(Exception ex)
+            {
+                //Se acaso der algum erro
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnLocalizar_Click(object sender, EventArgs e)
+        {
+            //Para pegar o anexo.
+            ofdProcurar.ShowDialog();
+            txtAnexo.Text = ofdProcurar.FileName;
+        }
+
+        /// <summary>
+        /// Cria o corpo da página.
+        /// </summary>
+        /// <param name="nome">Recebe o nome do destinatário.</param>
+        /// <param name="mensagem">Recebe uma mensagem escrita.</param>
+        /// <returns>Retorna o corpo com as informações inseridas.</returns>
+        public string corpoDaMensagem(string nome, string mensagem )
+        {
+            string a = "c1.staticflickr.com/5/4185/34282917092_d57050bf2c_b.jpg";
+            string body = "<div style=\"font-family: Arial, Helvetica, sans-serif;\">" +
+                                "E-mail de teste do 'Programa para enviar e-mail' em c#<br/><br/>" +
+                                "<table width=\"429\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border: 2px solid #09F;text-align:center;\">" +
+                                    "<tr>" +
+                                        "<td width=\"397\" height=\"93\" style=\"border: 2px solid #09F; font-size:20px;color: " +
+                                        "#FFF; background:#09F\"><b> Nome do contato:</b> " + txtNome.Text + "</td>" +
+                                    "</tr>" +
+                                    "<tr>" +
+                                        "<td style=\"border: 2px solid #09F; padding:10px; text-align:left;\"><p><b>Mensagem:</b></p>" +
+                                            "<p>" + txtMensagem.Text + "</p></td>" +
+                                    "</tr>" +
+                                    "<tr>" +
+                                        "<td style=\"border: 2px solid #09F; background:#0CF; padding:10px;\"><p><b>Imagem modelo:</b></p>" +
+                                            "<p><img src=\"http://" + a + " \" width=\"313\" height=\"204\" /></p></td>" +
+                                    "</tr>" +
+                                "</table>" +
+                            "</div>";
+            return body;
+        }
+
+        /// <summary>
+        /// Limpa as caixa de textos do formulário.
+        /// </summary>
+        public void limparCaixasDeTextos()
+        {
+            txtNome.Text = "";
+            txtPara.Text = "";
+            txtAnexo.Text = "";
+            txtAssunto.Text = "";
+            txtMensagem.Text = "";
+        }
+    }
+}
